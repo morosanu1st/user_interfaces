99220ea8e3e3401b20ba73768a05475e42dd90af
mihai chindris <morosanu1st@yahoo.com>
2018-12-08 14:58:54 +0100

    almost implemeted login

--- js/main.js
+++ js/main.js
@@ -2,7 +2,24 @@ $(document).ready(function() {
     $(".search-hotels-button").click(function(e) {
         $("#search-hotels").css("display", "none");
         $("#hotel-results").fadeIn("slow");
+        $("#new-search").fadeIn("fast");
     });
 
+    $("#new-search").click(function() {
+        $("#hotel-results").css("display", "none");
+        $("#new-search").fadeOut("fast");
+        $("#search-hotels").fadeIn("slow");
+    });
 
 })
+
+// function tryLogin() {
+
+//     if ($("#login")[0].checkValidity()) {
+//         console.log("yay");
+//         window.location.href = "home-logged-in.html";
+//     } else {
+//         console.log("ny")
+//         $("#login").trigger('submit');
+//     }
+// }
