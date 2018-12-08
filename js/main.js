32a440ac0eae840b51ecd93927cbba6d30296466
kaddour97 <36898215+kaddour97@users.noreply.github.com>
2018-12-08 22:26:40 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
     $(".search-hotels-button").click(function(e) {
         $("#search-hotels").css("display", "none");
@@ -6,7 +5,27 @@ $(document).ready(function() {
 		$("#waldorfHotelInformation").css("display", "none");
 		$("#exeMoncloaHotelInformation").css("display", "none");
 		$("#versallesHotelInformation").css("display", "none");
+		$("#travelMantaInformation").css("display", "none");
+		$("#travelRomeInformation").css("display", "none");
+		$("#travelSailDayInformation").css("display", "none");
+		$("#travelMexicoInformation").css("display", "none");
         $("#hotel-results").fadeIn("slow");
+    });
+
+
+})
+$(document).ready(function() {															
+    $(".search-travels-button").click(function(a) {
+        $("#search-travels").css("display", "none");
+		$("#conradHotelInformation").css("display", "none");
+		$("#waldorfHotelInformation").css("display", "none");
+		$("#exeMoncloaHotelInformation").css("display", "none");
+		$("#versallesHotelInformation").css("display", "none");
+		$("#travelMantaInformation").css("display", "none");
+		$("#travelRomeInformation").css("display", "none");
+		$("#travelSailDayInformation").css("display", "none");
+		$("#travelMexicoInformation").css("display", "none");
+        $("#travel-results").fadeIn("slow");
     });
 
 
@@ -22,7 +41,13 @@ function showTravelExperiences(){
         $("#search-hotels").css("display", "none");
 		$("#collaborative-list").css("display", "none");
 		$("#search-box").css("display", "none");
+		$("#travelMantaInformation").css("display", "none");
+		$("#travelRomeInformation").css("display", "none");
+		$("#travelSailDayInformation").css("display", "none");
+		$("#travelMexicoInformation").css("display", "none");
+		$("#travel-results").css("display", "none");
 		$("#travel-experiences").fadeIn("slow");
+		$("#search-travels").fadeIn("slow");
   }
 
   function showCollaborativeList(){
@@ -35,6 +60,10 @@ function showTravelExperiences(){
         $("#search-hotels").css("display", "none");
 		$("#travel-experiences").css("display", "none");
 		$("#search-box").css("display", "none");
+		$("#travelMantaInformation").css("display", "none");
+		$("#travelRomeInformation").css("display", "none");
+		$("#travelSailDayInformation").css("display", "none");
+		$("#travelMexicoInformation").css("display", "none");
 		$("#collaborative-list").fadeIn("slow");
   }
 
@@ -47,6 +76,10 @@ function showHotelSearch(){
 		$("#versallesHotelInformation").css("display", "none");
 		$("#collaborative-list").css("display", "none");
 		$("#travel-experiences").css("display", "none");
+		$("#travelMantaInformation").css("display", "none");
+		$("#travelRomeInformation").css("display", "none");
+		$("#travelSailDayInformation").css("display", "none");
+		$("#travelMexicoInformation").css("display", "none");
 		$("#search-box").fadeIn("slow");
         $("#search-hotels").fadeIn("slow");
   }
@@ -60,6 +93,10 @@ function showFavouriteList(){
 	 $("#exeMoncloaHotelInformation").css("display", "none");
 	 $("#versallesHotelInformation").css("display", "none");
 	 $("#travel-experiences").css("display", "none");
+	 $("#travelMantaInformation").css("display", "none");
+	 $("#travelRomeInformation").css("display", "none");
+	 $("#travelSailDayInformation").css("display", "none");
+	 $("#travelMexicoInformation").css("display", "none");
 	 $("#collaborative-list").css("display", "none");
 	 $("#search-box").css("display", "none");
 	 $("#favourite-list").fadeIn("slow");
@@ -78,7 +115,7 @@ function deleteVersallesFavourite(){
 	  document.getElementById('versallesFavourite').style.display='none';
 }
 //Functions for adding the hotels to the favourite list
-function deleteConradFavourite(){
+function addConradFavourite(){
 	  document.getElementById('conradFavourite').style.display='block';
 }
 function addWaldorfFavourite(){
@@ -91,31 +128,371 @@ function addVersallesFavourite(){
 	  document.getElementById('versallesFavourite').style.display='block';
 }
 
-function startFav(){
-	document.getElementById('exeMoncloaFavourite').style.display='none';
-	document.getElementById('versallesFavourite').style.display='none';
-}	
 
 //Functions for showing the full information of  a hotel
 function showConradInfo(){
 	 $("#hotel-results").css("display", "none");
 	 $("#search-box").css("display", "none");
+	 $("#favourite-list").css("display", "none");
 	 $("#conradHotelInformation").fadeIn("slow");
 
 }
 function showWaldorfInfo(){
 	 $("#hotel-results").css("display", "none");
 	 $("#search-box").css("display", "none");
+	 $("#favourite-list").css("display", "none");
 	 $("#waldorfHotelInformation").fadeIn("slow");
 }
 function showExeMoncloaInfo(){
 	 $("#hotel-results").css("display", "none");
 	 $("#search-box").css("display", "none");
+	 $("#favourite-list").css("display", "none");
 	 $("#exeMoncloaHotelInformation").fadeIn("slow");
 }
 function showVersallesInfo(){
 	 $("#hotel-results").css("display", "none");
 	 $("#search-box").css("display", "none");
+	 $("#favourite-list").css("display", "none");
 	 $("#versallesHotelInformation").fadeIn("slow");
+}
+
+//Functions to show comments or ratings of a hotel
+function showCommentsConrad(){
+	 $("#ratingsConrad").css("display", "none");
+	 $("#commentsConrad").fadeIn("slow");
+}
+function showCommentsWaldorf(){
+	 $("#ratingsWaldorf").css("display", "none");
+	 $("#commentsWaldorf").fadeIn("slow");
+}
+function showCommentsExeMoncloa(){
+	 $("#ratingsExeMoncloa").css("display", "none");
+	 $("#commentsExeMoncloa").fadeIn("slow");
+}
+function showCommentsVersalles(){
+	 $("#ratingsVersalles").css("display", "none");
+	 $("#commentsVersalles").fadeIn("slow");
+}
+function showRatingsConrad(){
+	 $("#commentsConrad").css("display", "none");
+	 $("#ratingsConrad").fadeIn("slow");
+}
+function showRatingsWaldorf(){
+	 $("#commentsWaldorf").css("display", "none");
+	 $("#ratingsWaldorf").fadeIn("slow");
+}
+function showRatingsExeMoncloa(){
+	 $("#commentsExeMoncloa").css("display", "none");
+	 $("#ratingsExeMoncloa").fadeIn("slow");
+}
+function showRatingsVersalles(){
+	 $("#commentsVersalles").css("display", "none");
+	 $("#ratingsVersalles").fadeIn("slow");
+}
+//Functions for showing the full information of a travel experiences
+function showNightDiveInfo(){
+	$("#travel-results").css("display", "none");
+	$("#search-box").css("display", "none");
+	$("#favourite-list").css("display", "none");
+	$("#travelMantaInformation").fadeIn("slow");
+}
+
+function showRomeInfo(){
+	$("#travel-results").css("display", "none");
+	$("#favourite-list").css("display", "none");
+	$("#search-box").css("display", "none");
+	$("#travelRomeInformation").fadeIn("slow");
+}
+
+function showSailDayInfo(){
+	$("#travel-results").css("display", "none");
+	$("#search-box").css("display", "none");
+	$("#favourite-list").css("display", "none");
+	$("#travelSailDayInformation").fadeIn("slow");
+}
+
+function showMexicoInfo(){
+	$("#travel-results").css("display", "none");
+	$("#search-box").css("display", "none");
+	$("#favourite-list").css("display", "none");
+	$("#travelMexicoInformation").fadeIn("slow");
+}
+
+//Function for showing the lists 
+function myLists(){
+	var x = document.getElementById("lists").value;
+    if (x=="List 1"){
+		showList1();
+	}
+	if (x=="List 2"){
+		showList2();
+	}
+	if (x=="List 3"){
+		showList3();
+	}
+}
+
+function showList1(){
+	$(".list").css("display", "none");
+	$("#list1").fadeIn("slow");
+}
+
+function showList2(){
+	$(".list").css("display", "none");
+	$("#list2").fadeIn("slow");
+}
+
+function showList3(){
+	$(".list").css("display", "none");
+	$("#list3").fadeIn("slow");
+}
+
+//Function for showing and hiding the users of the collaborative lists
+
+function showUsers(){
+	$(".users").css("padding-right", "1%");
+	var x = document.getElementById("lists").value;
+	if (x=="List 1"){
+		showUsersList1();
+	}
+	if (x=="List 2"){
+		showUsersList2();
+	}
+	if (x=="List 3"){
+		showUsersList3();
+	}
+}
+
+function hideUsers(){
+	$(".user-list").css("display", "none");
+	$(".users").css("padding-right", "18%");
+}
+
+function showUsersList1(){
+	$(".user-list").css("display", "none");
+	$("#user-list1").fadeIn("slow");
+}
+
+function showUsersList2(){
+	$(".user-list").css("display", "none");
+	$("#user-list2").fadeIn("slow");
+}
+
+function showUsersList3(){
+	$(".user-list").css("display", "none");
+	$("#user-list3").fadeIn("slow");
+}
+
+//functions for deleting travele experiences or hotels from the collaborative lists
+//LIST 1
+function deleteConradList1(){
+	document.getElementById('conradList1').style.display='none';
+}
+function deleteWaldorfList1(){
+	document.getElementById('waldorfList1').style.display='none';
+}
+function deleteExeMoncloaList1(){
+	document.getElementById('exeMoncloaList1').style.display='none';
+}
+function deleteVersallesList1(){
+	document.getElementById('versallesList1').style.display='none';
+}
+function deleteMantaList1(){
+	document.getElementById('mantaList1').style.display='none';
+}
+function deleteMexicoList1(){
+	document.getElementById('mexicoList1').style.display='none';
+}
+function deleteRomeList1(){
+	document.getElementById('romeList1').style.display='none';
+}
+function deleteCamdenList1(){
+	document.getElementById('camdenList1').style.display='none';
+}
+//LIST 2
+function deleteConradList2(){
+	document.getElementById('conradList2').style.display='none';
+}
+function deleteWaldorfList2(){
+	document.getElementById('waldorfList2').style.display='none';
+}
+function deleteExeMoncloaList2(){
+	document.getElementById('exeMoncloaList2').style.display='none';
+}
+function deleteVersallesList2(){
+	document.getElementById('versallesList2').style.display='none';
+}
+function deleteMantaList2(){
+	document.getElementById('mantaList2').style.display='none';
+}
+function deleteMexicoList2(){
+	document.getElementById('mexicoList2').style.display='none';
+}
+function deleteRomeList2(){
+	document.getElementById('romeList2').style.display='none';
+}
+function deleteCamdenList2(){
+	document.getElementById('camdenList2').style.display='none';
+}
+//LIST 3
+function deleteConradList3(){
+	document.getElementById('conradList3').style.display='none';
+}
+function deleteWaldorfList3(){
+	document.getElementById('waldorfList3').style.display='none';
+}
+function deleteExeMoncloaList3(){
+	document.getElementById('exeMoncloaList3').style.display='none';
+}
+function deleteVersallesList3(){
+	document.getElementById('versallesList3').style.display='none';
+}
+function deleteMantaList3(){
+	document.getElementById('mantaList3').style.display='none';
+}
+function deleteMexicoList3(){
+	document.getElementById('mexicoList3').style.display='none';
+}
+function deleteRomeList3(){
+	document.getElementById('romeList3').style.display='none';
+}
+function deleteCamdenList3(){
+	document.getElementById('camdenList3').style.display='none';
+}
+//functions for adding travele experiences or hotels from the collaborative lists
+//LIST 1
+function addConradList1(){
+	document.getElementById('conradList1').style.display='block';
+}
+function addWaldorfList1(){
+	document.getElementById('waldorfList1').style.display='block';
+}
+function addExeMoncloaList1(){
+	document.getElementById('exeMoncloaList1').style.display='block';
+}
+function addVersallesList1(){
+	document.getElementById('versallesList1').style.display='block';
+}
+function addMantaList1(){
+	document.getElementById('mantaList1').style.display='block';
+}
+function addMexicoList1(){
+	document.getElementById('mexicoList1').style.display='block';
+}
+function addRomeList1(){
+	document.getElementById('romeList1').style.display='block';
+}
+function addCamdenList1(){
+	document.getElementById('camdenList1').style.display='block';
+}
+//LIST 2
+function addConradList2(){
+	document.getElementById('conradList2').style.display='block';
+}
+function addWaldorfList2(){
+	document.getElementById('waldorfList2').style.display='block';
+}
+function addExeMoncloaList2(){
+	document.getElementById('exeMoncloaList2').style.display='block';
+}
+function addVersallesList2(){
+	document.getElementById('versallesList2').style.display='block';
+}
+function addMantaList2(){
+	document.getElementById('mantaList2').style.display='block';
+}
+function addMexicoList2(){
+	document.getElementById('mexicoList2').style.display='block';
+}
+function addRomeList2(){
+	document.getElementById('romeList2').style.display='block';
+}
+function addCamdenList2(){
+	document.getElementById('camdenList2').style.display='block';
+}
+//LIST 3
+function addConradList3(){
+	document.getElementById('conradList3').style.display='block';
+}
+function addWaldorfList3(){
+	document.getElementById('waldorfList3').style.display='block';
+}
+function addExeMoncloaList3(){
+	document.getElementById('exeMoncloaList3').style.display='block';
+}
+function addVersallesList3(){
+	document.getElementById('versallesList3').style.display='block';
+}
+function addMantaList3(){
+	document.getElementById('mantaList3').style.display='block';
+}
+function addMexicoList3(){
+	document.getElementById('mexicoList3').style.display='block';
+}
+function addRomeList3(){
+	document.getElementById('romeList3').style.display='block';
+}
+function addCamdenList3(){
+	document.getElementById('camdenList3').style.display='block';
+}
+
+//Functions for deleting the travel experiences from the favourote list
+function deleteMantaFavourite(){
+	  document.getElementById('mantaFavourite').style.display='none';
+}
+function deleteCamdenFavourite(){
+	  document.getElementById('camdenFavourite').style.display='none';
+}
+function deleteMexicoFavourite(){
+	  document.getElementById('mexicoFavourite').style.display='none';
+}
+function deleteRomeFavourite(){
+	  document.getElementById('romeFavourite').style.display='none';
+}
+//Functions for adding the travel expereicnes to the favourite list
+function addMantaFavourite(){
+	  document.getElementById('mantaFavourite').style.display='block';
+}
+function addCamdenFavourite(){
+	  document.getElementById('camdenFavourite').style.display='block';
+}
+function addMexicoFavourite(){
+	  document.getElementById('mexicoFavourite').style.display='block';
+}
+function addRomeFavourite(){
+	  document.getElementById('romeFavourite').style.display='block';
+}
+//functions for showing the list to which we can add the hotel or travel experience
+function ShowListsConrad(){
+	document.getElementById('addToListConrad').style.display='none';
+	document.getElementById('conradLists').style.display='block';
+}
+function ShowListsWaldorf(){
+	document.getElementById('addToListWaldorf').style.display='none';
+	document.getElementById('waldorfLists').style.display='block';
+}
+function ShowListsExeMoncloa(){
+	document.getElementById('addToListExeMoncloa').style.display='none';
+	document.getElementById('exeMoncloaLists').style.display='block';
+}
+function ShowListsVersalles(){
+	document.getElementById('addToListVersalles').style.display='none';
+	document.getElementById('versallesLists').style.display='block';
+}
+function ShowListsRome(){
+	document.getElementById('addToListRome').style.display='none';
+	document.getElementById('romeLists').style.display='block';
+}
+function ShowListsManta(){
+	document.getElementById('addToListManta').style.display='none';
+	document.getElementById('mantaLists').style.display='block';
+}
+function ShowListsCamden(){
+	document.getElementById('addToListCamden').style.display='none';
+	document.getElementById('camdenLists').style.display='block';
+}
+function ShowListsMexico(){
+	document.getElementById('addToListMexico').style.display='none';
+	document.getElementById('mexicoLists').style.display='block';
 }
 
